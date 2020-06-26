da0c161b6909cda7496425afa93a48bc66ed4151
TheNameOfDreams <Blackwindow2109>
2020-06-26 15:37:17 -0300

    Transform Gladiator in git module

--- gladiator/src/main/java/tk/yallandev/saintmc/gladiator/challenge/ChallengeType.java
+++
@@ -1,8 +0,0 @@
-package tk.yallandev.saintmc.gladiator.challenge;
-
-public enum ChallengeType {
-	
-	SHADOW_CUSTOM, SHADOW_NORMAL, SHADOW_FAST, //SHADOW
-	SUMO_NORMAL; //SUMO
-
-}
--- gladiator/src/main/java/tk/yallandev/saintmc/gladiator/controller/ChallengeController.java
+++
@@ -1,5 +0,0 @@
-package tk.yallandev.saintmc.gladiator.controller;
-
-public class ChallengeController {
-
-}
